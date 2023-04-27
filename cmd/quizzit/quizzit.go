8b197838eacd8c524f091c5c7d1cdfbf5044bdde
h3rby7 <herbielutz@gmx.net>
2023-04-27 14:59:31 +0200

    keep DIR structure

--- cmd/quizzit/quizzit.go
+++ cmd/quizzit/quizzit.go
@@ -1,7 +1,89 @@
-package main
-
 //go:generate npm run regenerate:golang
 
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"gitlab.mi.hdm-stuttgart.de/quizzit/backend-server/types"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func writeWebsocketMessage(conn *websocket.Conn, msg types.WebsocketMessage) error {
+	data, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	err = conn.WriteMessage(websocket.TextMessage, []byte(string(data)))
+	return err
+}
+
+func healthCheckHttp(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Fprintf(w, "System is running...")
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func healthCheckWs(conn *websocket.Conn) {
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			err := writeWebsocketMessage(conn, types.WebsocketMessage{MessageType: types.HEALTH_MESSAGE, Data: types.HealthCheck{Healthy: true}})
+			if err != nil {
+				fmt.Println("Failed to send message:", err)
+				return
+			}
+		}
+	}
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, payload, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(payload), messageType)
+	}
+}
+
+func writer(conn *websocket.Conn) {
+	go healthCheckWs(conn)
+}
+
+func websocketEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Successfully connected...")
+	go reader(ws)
+	go writer(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/health", healthCheckHttp)
+	http.HandleFunc("/ws", websocketEndpoint)
+}
+
 func main() {
-	println("Hello from main (quizzit.go)")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
