9cf63b9d2e37a8cd0af063eb35509345f5bd1537
KaiKustermann <kai.kuster@gmx.de>
2023-05-17 11:57:48 +0200

    list of connected clients added

--- internal/websockets/WebsocketEndpoint.go
+++ internal/websockets/WebsocketEndpoint.go
@@ -2,6 +2,7 @@ package ws
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
@@ -11,9 +12,44 @@ import (
 	"gitlab.mi.hdm-stuttgart.de/quizzit/backend-server/internal/logging"
 )
 
+var clients []*websocket.Conn
+var clientsMutex sync.Mutex
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+}
+
+// Adds a client to the slice of connected clients
+func AddClient(conn *websocket.Conn) {
+	clientsMutex.Lock()
+	clients = append(clients, conn)
+	clientsMutex.Unlock()
+}
+
+// Removes a client to the slice of connected clients
+func RemoveClient(conn *websocket.Conn) {
+	var result []*websocket.Conn
+	for _, v := range clients {
+		if v != conn {
+			result = append(result, v)
+		}
+	}
+	clientsMutex.Lock()
+	clients = result
+	clientsMutex.Unlock()
+}
+
+// Broadcasts a message to all connected clients
+func BroadCastMessageToAllConnectedClients(msg dto.WebsocketMessageSubscribe) error {
+	for i := 0; i < len(clients); i++ {
+		err := helpers.WriteWebsocketMessage(clients[i], msg)
+		if err != nil {
+			log.Error("Message could not be send to client", err)
+			return err
+		}
+	}
+	return nil
 }
 
 func WebsocketEndpoint(w http.ResponseWriter, r *http.Request) {
@@ -29,13 +65,21 @@ func WebsocketEndpoint(w http.ResponseWriter, r *http.Request) {
 
 // Hook to do any work necessary when a client connects
 func onConnect(conn *websocket.Conn) {
+	AddClient(conn)
 	log.WithField("clientAddress", conn.RemoteAddr()).Info("New connection from client")
-	helpers.WriteWebsocketMessage(conn, GetNextQuestionMessage())
+	BroadCastMessageToAllConnectedClients(GetNextQuestionMessage())
+}
+
+// Hook to do any work necessary when a client disconnects
+func onClose(conn *websocket.Conn) {
+	RemoveClient(conn)
+	log.WithField("clientAddress", conn.RemoteAddr()).Info("Connection closed")
 }
 
 // Listens for incoming Messages on the Websocket
 // Expects messages to be of type dto.WebsocketMessagePublish
 func listen(conn *websocket.Conn) {
+	defer onClose(conn)
 	for {
 		var handled = false
 		_, payload, err := conn.ReadMessage()
