bd7ee0dd407e2e1229ef375267ba24f3bdce0c0f
KaiKustermann <kai.kuster@gmx.de>
2023-06-11 17:30:44 +0200

    Logic from StaticQuestions.go was added to the questionManager, for instance getting the next question.

--- internal/game/questionManager.go
+++ internal/game/questionManager.go
@@ -7,35 +7,51 @@ import (
 
 // Statefully handle the catalog of questions and the active question
 type questionManager struct {
-	questions      question.Questions
-	activeQuestion dto.Question
+	questions           []question.Question
+	activeQuestion      question.Question
+	activeQuestionIndex int
 }
 
 // Constructs a new QuestionManager
 func NewQuestionManager() (qc questionManager) {
 	qc = questionManager{
-		questions: question.MakeStaticQuestions(),
+		questions: question.LoadQuestions(),
 	}
 	return
 }
 
 // Retrieve the currently active question
-func (qc *questionManager) GetActiveQuestion() dto.Question {
+func (qc *questionManager) GetActiveQuestion() question.Question {
 	return qc.activeQuestion
 }
 
 // Move on to the next question and return it
-func (qc *questionManager) MoveToNextQuestion() dto.Question {
-	qc.setActiveQuestion(qc.questions.GetNextQuestion())
+func (qc *questionManager) MoveToNextQuestion() question.Question {
+	if qc.activeQuestionIndex+1 >= len(qc.questions) {
+		qc.activeQuestionIndex = 0
+	} else {
+		qc.activeQuestionIndex += 1
+	}
+	qc.setActiveQuestion(qc.questions[qc.activeQuestionIndex])
 	return qc.GetActiveQuestion()
 }
 
+// Get the index of the active question in the list of questions
+func (qc *questionManager) getActiveQuestionIndex() int {
+	for idx, q := range qc.questions {
+		if q.Id == qc.activeQuestion.Id {
+			return idx
+		}
+	}
+	return -1
+}
+
 // Setter for activeQuestion
-func (qc *questionManager) setActiveQuestion(question dto.Question) {
+func (qc *questionManager) setActiveQuestion(question question.Question) {
 	qc.activeQuestion = question
 }
 
-// Convenience passthrough to Questions.GetCorrectnessFeedback
+// Get the corrextness feedback for the active question
 func (qc *questionManager) GetCorrectnessFeedback(answer dto.SubmitAnswer) dto.CorrectnessFeedback {
-	return qc.questions.GetCorrectnessFeedback(answer)
+	return qc.activeQuestion.GetCorrectnessFeedback(answer)
 }
