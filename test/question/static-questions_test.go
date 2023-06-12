22855ceff228dc465220fe8ef2559e973b292cf1
KaiKustermann <kai.kuster@gmx.de>
2023-06-12 09:56:35 +0200

    Logic for loading question into question manager

--- test/question/static-questions_test.go
+++ test/question/static-questions_test.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"testing"
 
-	"gitlab.mi.hdm-stuttgart.de/quizzit/backend-server/internal/question"
+	gameobjects "gitlab.mi.hdm-stuttgart.de/quizzit/backend-server/internal/game/game-objects"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestQuestions(t *testing.T) {
-	questions := question.MakeStaticQuestions()
+	questions := gameobjects.MakeStaticQuestions()
 	for i := 0; i < 5; i++ {
 		q := questions.GetNextQuestion()
 		assert.Equal(t, fmt.Sprintf("question-%d", i), q.Id)
