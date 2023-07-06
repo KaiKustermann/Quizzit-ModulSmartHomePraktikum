91737a6bf8c758c4d2e9fdb6abcbe1ebf3b23aca
h3rby7 <herbielutz@gmx.net>
2023-07-06 12:01:40 +0200

    use limited broadcast (local broadcast)

--- internal/hybrid-die/finder.go
+++ internal/hybrid-die/finder.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
-	"gopkg.in/netaddr.v1"
 )
 
 type HybridDieFinder struct {
@@ -17,7 +16,7 @@ func (bc *HybridDieFinder) Start() {
 	log.Info("Starting to find a hybrid die")
 	bc.isBroadcasting = true
 	for i := 0; bc.isBroadcasting; i++ {
-		bc.broadcastOnAllInterfaces(i)
+		bc.sendLimitedBroadcast(i)
 		time.Sleep(3 * time.Second)
 	}
 	log.Info("Stopped finding a hybrid die")
@@ -28,76 +27,37 @@ func (bc *HybridDieFinder) Stop() {
 	bc.isBroadcasting = false
 }
 
-func (bc *HybridDieFinder) broadcastOnAllInterfaces(attempt int) {
-	cL := log.WithField("attempt", attempt)
-	cL.Infof("Finding hybrid die ")
-	addrs := bc.getBroadcastAddresses(attempt)
-	for _, addr := range addrs {
-		bc.sendUDP(addr, attempt)
-	}
-}
-
-func (bc *HybridDieFinder) sendUDP(ip net.IP, attempt int) {
+func (bc *HybridDieFinder) sendLimitedBroadcast(attempt int) {
 	network := "udp4"
 	port := ":7777"
 	connectionCallWord := "SuperDuperDiceConnectionCall"
 	cL := log.WithFields(log.Fields{
 		"attempt": attempt,
-		"ip":      ip,
 	})
-	log.Debugf("Resolving own %s adress for port %s", network, port)
+	cL.Debugf("Finding hybrid die ")
+	cL.Tracef("Resolving own %s adress for port %s ", network, port)
 	local, err := net.ResolveUDPAddr(network, port)
 	if err != nil {
 		cL.Warn(err)
 		return
 	}
-	log.Debugf("Resolving target %s adress for port %s", network, port)
-	remote, err := net.ResolveUDPAddr(network, fmt.Sprintf("%s%s", ip.String(), port))
+	cL.Tracef("Resolving target %s adress for port %s ", network, port)
+	remote, err := net.ResolveUDPAddr(network, fmt.Sprintf("%s%s", net.IPv4bcast.String(), port))
 	if err != nil {
 		cL.Warn(err)
 		return
 	}
-	log.Debugf("Creating UDP connection for %s from %s:%d to %s:%d", network, local.IP, local.Port, remote.IP, remote.Port)
+	cL.Tracef("Creating UDP connection for %s from %s:%d to %s:%d ", network, local.IP, local.Port, remote.IP, remote.Port)
 	list, err := net.DialUDP(network, local, remote)
 	if err != nil {
 		cL.Warn(err)
 		return
 	}
 	defer list.Close()
-	log.Debugf("Writing '%s' to UDP connection", connectionCallWord)
+	cL.Tracef("Writing '%s' to UDP connection ", connectionCallWord)
 	_, err = list.Write([]byte(connectionCallWord))
 	if err != nil {
 		cL.Warn(err)
 		return
 	}
-}
-
-func (bc *HybridDieFinder) getBroadcastAddresses(attempt int) (bcAddrs []net.IP) {
-	cL := log.WithField("attempt", attempt)
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		cL.Error(err)
-		return
-	}
-	cL.Tracef("Iterating through system interfaces ")
-	for _, i := range ifaces {
-		// https://github.com/IBM/netaddr/blob/master/net_utils.go
-		addrs, err := i.Addrs()
-		if err != nil {
-			cL.Warn(err)
-			continue
-		}
-		cL.WithField("addresses", addrs).Debugf("Found %d addresses ", len(addrs))
-		for _, addr := range addrs {
-			switch v := addr.(type) {
-			case *net.IPNet:
-				bcAddrs = append(bcAddrs, netaddr.BroadcastAddr(v))
-			case *net.IPAddr:
-				cL.Tracef("Skipping IPV6 addr -> %s", v.IP)
-			default:
-				cL.Debugf("Skipping unkown address type -> network: %s, address: %s", v.Network(), v.String())
-			}
-		}
-	}
-	return
 }
