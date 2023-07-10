5c40fc2b0018ea688f78555bce3bea099d8504d1
h3rby7 <herbielutz@gmx.net>
2023-07-10 14:50:11 +0200

    fix bug where we would not stop broadcasting

--- internal/hybrid-die/connector.go
+++ internal/hybrid-die/connector.go
@@ -5,17 +5,17 @@ import (
 )
 
 type HybridDie struct {
-	finder     HybridDieFinder
-	controller HybridDieController
+	finder     *HybridDieFinder
+	controller *HybridDieController
 }
 
 func NewHybridDie() HybridDie {
-	finder := HybridDieFinder{}
+	finder := NewHybridDieFinder()
 	return HybridDie{
-		finder: finder,
-		controller: HybridDieController{
-			onDieFound: finder.Stop,
-			onDieLost:  finder.Start,
+		finder: &finder,
+		controller: &HybridDieController{
+			callbackOnDieFound: func() { finder.Stop() },
+			callbackOnDieLost:  func() { finder.Start() },
 		},
 	}
 }
