20c1e0f75429d646d01d035821dd982da92610fe
KaiKustermann <kai.kuster@gmx.de>
2023-07-12 17:56:30 +0200

    new MessageTypesSubscribe Game_Reminder_RemindPlayerColorPrompt added

--- internal/message-types/MessageTypesSubscribe.go
+++ internal/message-types/MessageTypesSubscribe.go
@@ -3,15 +3,16 @@ package messagetypes
 type MessageTypeSubscribe string
 
 const (
-	System_Health                     MessageTypeSubscribe = "system/Health"
-	Game_Generic_ErrorFeedback        MessageTypeSubscribe = "game/generic/ErrorFeedback"
-	Game_Question_Question            MessageTypeSubscribe = "game/question/Question"
-	Game_Question_CorrectnessFeedback MessageTypeSubscribe = "game/question/CorrectnessFeedback"
-	Game_Setup_Welcome                MessageTypeSubscribe = "game/setup/Welcome"
-	Game_Setup_SelectPlayerCount      MessageTypeSubscribe = "game/setup/SelectPlayerCount"
-	Game_Turn_PassToSpecificPlayer    MessageTypeSubscribe = "game/turn/PassToSpecificPlayer"
-	Game_Die_RollCategoryPrompt       MessageTypeSubscribe = "game/die/RollCategoryPrompt"
-	Game_Die_CategoryResult           MessageTypeSubscribe = "game/die/CategoryResult"
-	Game_Turn_PassToNewPlayer         MessageTypeSubscribe = "game/die/PassToNewPlayer"
-	Game_Turn_NewPlayerColorPrompt    MessageTypeSubscribe = "game/die/NewPlayerColorPrompt"
+	System_Health                         MessageTypeSubscribe = "system/Health"
+	Game_Generic_ErrorFeedback            MessageTypeSubscribe = "game/generic/ErrorFeedback"
+	Game_Question_Question                MessageTypeSubscribe = "game/question/Question"
+	Game_Question_CorrectnessFeedback     MessageTypeSubscribe = "game/question/CorrectnessFeedback"
+	Game_Setup_Welcome                    MessageTypeSubscribe = "game/setup/Welcome"
+	Game_Setup_SelectPlayerCount          MessageTypeSubscribe = "game/setup/SelectPlayerCount"
+	Game_Turn_PassToSpecificPlayer        MessageTypeSubscribe = "game/turn/PassToSpecificPlayer"
+	Game_Die_RollCategoryPrompt           MessageTypeSubscribe = "game/die/RollCategoryPrompt"
+	Game_Die_CategoryResult               MessageTypeSubscribe = "game/die/CategoryResult"
+	Game_Turn_PassToNewPlayer             MessageTypeSubscribe = "game/die/PassToNewPlayer"
+	Game_Turn_NewPlayerColorPrompt        MessageTypeSubscribe = "game/die/NewPlayerColorPrompt"
+	Game_Reminder_RemindPlayerColorPrompt MessageTypeSubscribe = "game/die/RemindPlayerColorPrompt"
 )
