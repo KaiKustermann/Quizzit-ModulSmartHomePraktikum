80a8fa7ff00fc25e5c5f02ce5286ce2e2a9a1d1e
h3rby7 <herbielutz@gmx.net>
2024-01-17 00:56:48 +0100

    fix: game.questions not being patched in yaml file config patcher

--- internal/configuration/file/patcher.go
+++ internal/configuration/file/patcher.go
@@ -26,13 +26,36 @@ func PatchWithYAMLFile(conf *configmodel.QuizzitConfig, relPath string) (err err
 
 // patchGame patches the GameConfig field
 //
-// Only applies the patch, if the value of the 'file' config is not nil
-func patchGame(conf *configmodel.GameConfig, file *GameYAML) {
-	if file == nil || file.ScoredPointsToWin == nil {
+// Only applies patches, if the value of the 'game' config is not nil
+func patchGame(conf *configmodel.GameConfig, game *GameYAML) {
+	if game == nil {
+		log.Debug("Game is nil, not patching")
+		return
+	}
+	patchScoredPointsToWin(conf, game.ScoredPointsToWin)
+	patchQuestionsPath(conf, game.QuestionsPath)
+}
+
+// patchScoredPointsToWin patches the ScoredPointsToWin field
+//
+// Only applies the patch, if 'pointsToWin' is not nil
+func patchScoredPointsToWin(conf *configmodel.GameConfig, pointsToWin *int) {
+	if pointsToWin == nil {
 		log.Debug("ScoredPointsToWin is nil, not patching")
 		return
 	}
-	conf.ScoredPointsToWin = *file.ScoredPointsToWin
+	conf.ScoredPointsToWin = *pointsToWin
+}
+
+// patchQuestionsPath patches the QuestionsPath field
+//
+// Only applies the patch, if 'questionsPath' is not nil
+func patchQuestionsPath(conf *configmodel.GameConfig, questionsPath *string) {
+	if questionsPath == nil {
+		log.Debug("QuestionsPath is nil, not patching")
+		return
+	}
+	conf.QuestionsPath = *questionsPath
 }
 
 // patchHttp patches the HttpConfig field
